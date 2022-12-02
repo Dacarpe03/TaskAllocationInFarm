c581f2a558be9a0bca9c9cba6132f096fe94d479
Dacarpe03 <d.carmonap97@gmail.com>
2022-12-02 19:35:04 +0100

    Create new scripts for the cells

---
+++ Assets/Scripts/AbstractCellScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractCellScript : MonoBehaviour
+{
+    [Header("NextObject")]
+    [SerializeField] protected GameObject nextCellStateObject;
+
+    [Header("AnimationObject")]
+    [SerializeField] protected GameObject animationObject;
+
+    [Header("Timers")]
+    [SerializeField] protected float timeToNextState = 2f;
+
+    protected CellSpawnerScript a;
+
+    protected virtual void Start(){
+        a = FindObjectOfType<CellSpawnerScript>();
+        Debug.Log("Lo tengo");
+
+    }
+
+    protected void CreateTask(){
+        Debug.Log("Creating task");
+    }
+
+    protected void TriggerNextState(){
+        StartAnimation();
+        StartCoroutine(Countdown());
+        Destroy(this.gameObject);
+    }
+
+    protected void StartAnimation(){
+        //Instantiate(animationObject, this.transform.position, Quaternion.identity)
+    }
+
+    protected IEnumerator Countdown(){
+        yield return new WaitForSeconds(timeToNextState);
+        GameObject newState = Instantiate(nextCellStateObject, this.transform.position, Quaternion.identity);
+        //newState.GetComponent<AbstractCellScript>().SetScale(this.transform.localScale);
+    }
+
+    public void SetScale(Vector3 scale){
+        this.transform.localScale = scale;
+    }
+
+}
---
+++ Assets/Scripts/EmptyCellScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyCellScript : AbstractCellScript
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Debug.Log("Soy el hijo");
+        Debug.Log(timeToNextState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
