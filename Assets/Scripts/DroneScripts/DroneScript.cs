c25292b83bff910a1e871ccafb0c46c972f8fb67
Dacarpe03 <d.carmonap97@gmail.com>
2022-12-06 14:33:55 +0100

    Add function to get the a new task

--- Assets/Scripts/DroneScripts/DroneScript.cs
+++ Assets/Scripts/DroneScripts/DroneScript.cs
@@ -9,11 +9,18 @@ using UnityEngine;
 public class DroneScript : MonoBehaviour
 {   
     [Header("Drone parameters")]
-    [SerializeField] float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float height = 5f;
+
+    [Header("Attributes to control task")]
+    private Vector3 desiredPosition = new Vector3(10f, 10f, 10f);
+    private bool working = false;
 
     [Header("Tasks Queue")]
     private Queue<int[]> tasksQueue; // Tasks queue
 
+    [Header("Manager reference")]
+    private ManagerScript manager;
 
     /// <summary>
     /// Initializes the tasks queue
@@ -23,6 +30,51 @@ public class DroneScript : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// Finds a reference to the manager when the drown is instantiated
+    /// </summary>
+    private void Start(){
+        manager = FindObjectOfType<ManagerScript>();
+    }
+
+
+    /// <summary>
+    /// Checks if the drone can perform a new task
+    /// </summary>
+    private void Update(){
+        if (!working && tasksQueue.Count>0){
+            GetNextTask();
+        }
+    }
+
+    /// <summary>
+    /// Adds a task to the queue
+    /// </summary>
+    public void AddTask(int[] newTask){
+        tasksQueue.Enqueue(newTask);
+    }
+
+
+    private void GetNextTask(){
+        int[] nextTask = tasksQueue.Dequeue();
+        
+    }
+
+
+    /// <summary>
+    /// Moves to the correspoding cell
+    /// </summary>
+    private IEnumerator MoveToTaskPosition(){
+        if(Vector3.Distance(this.transform.position, desiredPosition) > 0.1f){
+            this.transform.position = Vector3.MoveTowards(this.transform.position, desiredPosition, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+            StartCoroutine(MoveToTaskPosition());
+        }else{
+            yield return null;
+            working = false;
+        }
+        
+    }
 
 
 }
