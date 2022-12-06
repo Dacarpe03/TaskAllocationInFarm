e0879310943d2cb646e904bb024cba14c2dd306f
Dacarpe03 <d.carmonap97@gmail.com>
2022-12-06 12:39:54 +0100

    Comment Manager

--- Assets/Scripts/CellScripts/WateredCellScript.cs
+++ Assets/Scripts/CellScripts/WateredCellScript.cs
@@ -4,6 +4,7 @@ public class WateredCellScript : AbstractCellScript
 {
     protected override void TriggerNextState(){
         base.TriggerNextState();
+        // Destroy the fruit of the object to get a better animation
         Object.Destroy(this.transform.GetChild(2).gameObject);
     }
 
--- Assets/Scripts/ManagerScript/ManagerScript.cs
+++ Assets/Scripts/ManagerScript/ManagerScript.cs
@@ -2,32 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Class to make a comunication tabloid between cells and drones
+/// </summary>
 public class ManagerScript : MonoBehaviour
 {
     [Header("Time to report")]
-    private float timeToReport = 10;
+    private float timeToReport = 10; // Time interval between countings
 
     [Header("Cells dictionary")]
-    Dictionary<int, AbstractCellScript> emptDict;
-    Dictionary<int, AbstractCellScript> seedsDict;
-    Dictionary<int, AbstractCellScript> wateredDict;
-    int cellsCount = 0;
+    Dictionary<int, AbstractCellScript> emptDict; // Empty cells dictionary
+    Dictionary<int, AbstractCellScript> seedsDict; // Seed cells dictionary
+    Dictionary<int, AbstractCellScript> wateredDict; // Watered cells dictionary
+    int cellsCount = 0; // This counter will serve as the id for a new cell
 
-    [Header("Cell Types")]
+    [Header("Cell Types")] // To know in which dictionary place the cell
     protected int emptyCell = 1;
     protected int seedCell = 2;
     protected int wateredCell = 3;
 
+
+    /// <summary>
+    /// First method to be invoked, will initialize the dictionaries
+    /// </summary>
     void Awake(){
         emptDict = new Dictionary<int, AbstractCellScript>();
         seedsDict = new Dictionary<int, AbstractCellScript>();
         wateredDict = new Dictionary<int, AbstractCellScript>();
     }   
     
+
+    /// <summary>
+    /// Begins the constant reporting
+    /// </summary>
     void Start(){
         StartCoroutine(ShowInfo());
     }
 
+
+    /// <summary>
+    /// Coroutine that executes in time intervals to follow the progress of the simulation
+    /// </summary>
     private IEnumerator ShowInfo(){
         string report = "Empty Cells: " + emptDict.Count.ToString();
         report += "\nSeed Cells:" + seedsDict.Count.ToString();
@@ -37,6 +53,19 @@ public class ManagerScript : MonoBehaviour
         StartCoroutine(ShowInfo());
     }
 
+
+    /// <summary> 
+    /// Adds a cell to the corresponding dict
+    /// </summary>
+    /// <param name="newCell">
+    /// AbstractCellScript to be added to the dictionary
+    /// </parm>
+    /// <param name="type">
+    /// int to know in which dictionary to store the new cell
+    /// </param>
+    /// <returns>
+    /// int as the cell id for the suscribed cell
+    /// </returns>
     public int AddCell(AbstractCellScript newCell, int type){
         if (type==emptyCell){
             emptDict[cellsCount] = newCell;
@@ -51,6 +80,16 @@ public class ManagerScript : MonoBehaviour
         return cellId;
     }
 
+
+    /// <summary> 
+    /// Removes a cell from the corresponding dict
+    /// </summary>
+    /// <param name="newCell">
+    /// AbstractCellScript to be removed from the dictionary
+    /// </parm>
+    /// <param name="type">
+    /// int to know from which dictionary remove the cell
+    /// </param>
     public void RemoveCell(int cellId, int type){
         if (type==emptyCell){
             emptDict.Remove(cellId);
